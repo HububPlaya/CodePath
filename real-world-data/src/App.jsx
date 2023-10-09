594532df53051eec24e3f2fbd1b39fac9ef00cab
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-10-09 15:50:52 -0500

    completed step 1

--- real-world-data/src/App.jsx
+++ real-world-data/src/App.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import './App.css'
 import APIForm from '../components/APIForm';
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
@@ -11,7 +12,24 @@ const App = () => {
     width: "",
     height: ""
   })
+  const submitForm = () => {}
   return (
-    <></>
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+      <APIForm 
+        inputs={inputs}
+        handleChange={(e) => 
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br/>
+    </div>
   )
 }
+
+
+export default App;
