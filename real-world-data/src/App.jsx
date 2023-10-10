0a8e0b5d2a8e7fc5f30457024dd1d07734656fbb
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-10-09 20:03:21 -0500

    almost finshed lab 4

--- real-world-data/src/App.jsx
+++ real-world-data/src/App.jsx
@@ -1,72 +1,81 @@
-import React,{useState} from 'react';
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 import APIForm from '../components/APIForm';
+
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 const App = () => {
-  const [currentImage, setCurrentImage] = useState(null)
+  const [prevImage, setPrevImage] = useState([])
+  const [currentImage, setCurrentImage] = useState(null);
   const [inputs, setInputs] = useState({
-    url: "",
-    format: "",
-    no_ads: "",
-    no_cookie_banners: "",
-    width: "",
-    height: ""
-  })
+    url: '',
+    format: '',
+    no_ads: '',
+    no_cookie_banners: '',
+    width: '',
+    height: '',
+  });
+
   const reset = () => {
     setInputs({
-      url: "",
-      format: "",
-      no_ads: "",
-      no_cookie_banners: "",
-      width: "",
-      height: ""
-    })
-  }
+      url: '',
+      format: '',
+      no_ads: '',
+      no_cookie_banners: '',
+      width: '',
+      height: '',
+    });
+  };
+
   const makeQuery = () => {
-    let wait_until = "network_idle";
-    let response_type = "json";
-    let fail_on_status = "400%2C404%2C500-511";
-    let url_starter = "https://";
-    let fullURL = url_starter + inputs.url;
-    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
-    callAPI(query).catch(console.error)
-  }
+    const wait_until = 'network_idle';
+    const response_type = 'json';
+    const fail_on_status = '400%2C404%2C500-511';
+    const url_starter = 'https://';
+    const fullURL = url_starter + inputs.url;
+    const query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+    callAPI(query).catch(console.error);
+  };
+
   const submitForm = () => {
-    let default_values = {
-      format: "jpeg",
-      no_ads: "true",
-      no_cookie_banners: "true",
-      width: "1920",
-      height: "1080"
-    }
-    if(inputs.url == "" || inputs.url == " ") {
-      alert("You forgot to submit a url!")
+    const default_values = {
+      format: 'jpeg',
+      no_ads: 'true',
+      no_cookie_banners: 'true',
+      width: '1920',
+      height: '1080',
+    };
+
+    if (!inputs.url.trim()) {
+      alert('You forgot to submit a URL!');
     } else {
-      for ( const[key, value] of Object.entries(inputs)) {
-        if(value == "") {
-          inputs[key] = default_values[key]
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value === '') {
+          inputs[key] = default_values[key];
         }
       }
-      makeQuery()
+      makeQuery();
     }
-  }
+  };
+
   const callAPI = async (query) => {
-    const response = await fetch(query)
-    const json = await response.json()
-    if(json.url == null) {
-      alert("Oops! Something went wrong with that query, lets try again!")
+    const response = await fetch(query);
+    const json = await response.json();
+    if (json.url === null) {
+      alert('Oops! Something went wrong with that query, let\'s try again!');
     } else {
-      setCurrentImage(json.url)
-      reset()
+      setCurrentImage(json.url);
+      setPrevImage((images) => [...images, json.url])
+      reset();
     }
-  }
+  };
+
   return (
     <div className="whole-page">
       <h1>Build Your Own Screenshot! 📸</h1>
-      <APIForm 
+      <APIForm
         inputs={inputs}
-        handleChange={(e) => 
+        handleChange={(e) =>
           setInputs((prevState) => ({
             ...prevState,
             [e.target.name]: e.target.value.trim(),
@@ -74,10 +83,29 @@ const App = () => {
         }
         onSubmit={submitForm}
       />
-      <br/>
+      {currentImage && (
+        <img
+          className="screenshot"
+          src={currentImage}
+          alt="screenshot returned"
+        />
+      )}
+      <div className="container">
+        <h3>Current Query Status: </h3>
+        <p>
+          https://api.apiflash.com/v1/urltoimage?access_key={ACCESS_KEY}
+          <br />
+          &url={inputs.url} <br />
+          &format={inputs.format} <br />
+          &width={inputs.width} <br />
+          &height={inputs.height} <br />
+          &no_cookie_banners={inputs.no_cookie_banners} <br />
+          &no_ads={inputs.no_ads} <br />
+        </p>
+      </div>
+      <br />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
