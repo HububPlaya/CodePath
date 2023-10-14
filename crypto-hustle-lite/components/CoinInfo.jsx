cfbeb0aebad1770e733159077f1baee77a957a8f
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-10-14 13:54:46 -0500

    checkpoint 2 complete

---
+++ crypto-hustle-lite/components/CoinInfo.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+
+function CoinInfo({image,name,sample}){
+    const[price,setPrice] = useState(null)
+    useEffect(() => {
+
+    }, [symbol]);
+} 
+
+export default CoinInfo
--- crypto-hustle-lite/src/App.jsx
+++ crypto-hustle-lite/src/App.jsx
@@ -1,29 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import CoinInfo from './components/CoinInfo';
+import './App.css';
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 const App = () => {
-  const API_KEY = import.meta.env.VITE_APP_API_KEY;
-  const [list, setList] = useState(null)
+  const [list, setList] = useState(null);
+
   useEffect(() => {
     async function fetchAllCoinData() {
-      const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist?&api_key"+ API_KEY)
-      const json = await response.json()
-      setList(json)
+      try {
+        const response = await fetch(
+          `https://min-api.cryptocompare.com/data/all/coinlist?api_key=${API_KEY}`
+        );
+        const json = await response.json();
+        setList(json);
+      } catch (error) {
+        console.error(error);
+      }
     }
-    fetchAllCoinData().catch(console.error);
-  }, [])
+
+    fetchAllCoinData();
+  }, []);
+
   return (
     <div className='whole-page'>
-  <h1>Crypto List</h1>
-  <ul>
-    {list && Object.entries(list.Data).map(([coin, data]) => 
-      data.PlatformType === "blockchain" ? (
-        <li key={data.FullName}>{data.FullName}</li>
-      ) : null
-    )}
-  </ul>
-</div>
+      <h1>Crypto List</h1>
+      <ul>
+        {list &&
+          Object.entries(list.Data).map(([coin, data]) =>
+            data.PlatformType === 'blockchain' ? (
+              <CoinInfo
+                key={coin}
+                image={data.ImageUrl}
+                name={data.FullName}
+                symbol={data.Symbol}
+              />
+            ) : null
+          )}
+      </ul>
+    </div>
+  );
+};
 
-  )
-}
-export default App
+export default App;
