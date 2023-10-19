85fca63c1ab36c4bba0813cc7371db3e72f7a1e2
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-10-18 19:01:38 -0500

    finsihed the lab

--- crypto-hustle-lite/src/App.jsx
+++ crypto-hustle-lite/src/App.jsx
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import CoinInfo from './components/CoinInfo';
+import SideNav from './components/SideNav';
 import './App.css';
+
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 const App = () => {
   const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) => 
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
 
   useEffect(() => {
     async function fetchAllCoinData() {
@@ -25,19 +44,33 @@ const App = () => {
 
   return (
     <div className='whole-page'>
+      <SideNav />
       <h1>Crypto List</h1>
+      <input
+  type="text"
+  placeholder="Search..."
+  onChange={(inputString) => searchItems(inputString.target.value)}
+/>
       <ul>
-        {list &&
-          Object.entries(list.Data).map(([coin, data]) =>
-            data.PlatformType === 'blockchain' ? (
-              <CoinInfo
-                key={coin}
-                image={data.ImageUrl}
-                name={data.FullName}
-                symbol={data.Symbol}
-              />
-            ) : null
-          )}
+      {searchInput.length > 0
+  ? filteredResults.map((coin) => 
+      list.Data[coin].PlatformType === "blockchain" ? 
+      <CoinInfo
+        image={list.Data[coin].ImageUrl}
+        name={list.Data[coin].FullName}
+        symbol={list.Data[coin].Symbol}
+      />
+      : null
+    )
+  : list && Object.entries(list.Data).map(([coin]) => 
+      list.Data[coin].PlatformType === "blockchain" ? 
+      <CoinInfo
+        image={list.Data[coin].ImageUrl}
+        name={list.Data[coin].FullName}
+        symbol={list.Data[coin].Symbol}
+      />
+  : null
+)}
       </ul>
     </div>
   );
---
+++ crypto-hustle-lite/src/components/SideNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CryptoScam from './CryptoScam';
+const SideNav = () => (
+  <div className="sidenav">
+    <CryptoScam />
+    <h1>here is my side nav bar</h1>
+  </div>
+);
+
+export default SideNav;
